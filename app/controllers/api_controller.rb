c9ba742748ff0d2ca1db60694097bd64cc77f262
Daniel <daniel@contadesk.com>
2017-10-09 20:23:24 -0400

    Cambios para final de primer fase

--- app/controllers/api_controller.rb
+++ app/controllers/api_controller.rb
@@ -78,7 +78,7 @@ class ApiController < ApplicationController
   end
 
   def verify_app_token
-    if params[:token]
+    if params[:token].nil?
        render json: {message: 'Forbidden'}, status: :unauthorized
     end
   end
