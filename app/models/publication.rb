110ebf34ab37e4dc9bc5b744fb4337a9d49c3fd1
Daniel <daniel@contadesk.com>
2017-10-16 20:48:08 -0400

    Se agrega configuracion de fecha y hora.

--- app/models/publication.rb
+++ app/models/publication.rb
@@ -4,6 +4,8 @@ class Publication < ApplicationRecord
     validates_presence_of :person_name
     has_many :comments
 
+    validates_presence_of :person_name, :photo, :age, :height, :weight, :date_of_dissapereance
+
     def sex_humanize
         return "Hombre" if self.sex.eql? 'male'
         return "Mujer" if self.sex.eql? 'female'
